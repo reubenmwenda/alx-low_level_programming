b2c772c8f7c886cede55aa9c5f15d075a16185f1
reubenmwenda <mwendaben97@gmail.com>
2022-10-15 07:47:21 -0700

    4-puts has been added1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 /**
- *
  * main - Entry point
  *
  * Return: Always 0 (Success)
- *
- *
  */
 int main(void)
 {
-	char my_string[] = "Programming is like building a multilingual puzzle\n";
+	char my_string[] = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(my_string);
 
