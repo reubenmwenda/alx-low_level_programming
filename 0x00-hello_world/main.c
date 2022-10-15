854dce0cc97aece390fcbe8650da839f3b5b81f3
reubenmwenda <mwendaben97@gmail.com>
2022-10-15 00:59:59 -0700

    file 0preprocessor has been created and corrected2

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -3,16 +3,15 @@
 
 /**
  *
- *  * main - Entry point
+ * main - Entry point
  *
- *   *
+ * Return: Always 0 (Success)
  *
- *    * Return: Always 0 (Success)
  *
- *     */
-
+ */
 int main(void)
 {
-	
-	return(0);
-}	
+
+	return (0);
+
+}
