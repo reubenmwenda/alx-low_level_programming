f073070c2c4fa03662dab75d35a2fbf0c619fd81
reubenmwenda <mwendaben97@gmail.com>
2022-10-15 07:40:57 -0700

    4-puts has been added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ *
+ */
+int main(void)
+{
+	char my_string[] = "Programming is like building a multilingual puzzle\n";
+
+	puts(my_string);
+
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,3 +1,5 @@
 0-preprocessor is for running through main
 1-compiler outputs with the file extension .o
 2-assembler outputs with the file extension .s
+3-was added to output file cisfun
+4-puts.c added to output some code
