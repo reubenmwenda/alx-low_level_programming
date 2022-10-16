86dd7cea83a2f6b4c59468ee756991a774cc8519
reubenmwenda <mwendaben97@gmail.com>
2022-10-16 10:48:27 -0700

    2-print_alphabet.c has been created

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - outputs alphabet letters
+ *
+ * Return: returns 0 (Success)
+ */
+int main(0)
+{
+	/* Simple code to show alphabet*/
+	char string = 'a';
+
+	while (string <= 'z')
+	{
+		putchar(string);
+
+		string++;
+
+	}
+
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,3 @@
 0-positive_or_negative.c is an if statement
 1-last_digit.c outputs using if statements with &&
+2-print_alphabet.c puts out alphabet
