a9546a493f576890bcda3fe1d128ccf41ad91e72
reubenmwenda <mwendaben97@gmail.com>
2022-10-19 03:11:02 -0700

    7-print_last_digit.c has been added

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,7 +10,14 @@
 int print_last_digit(int n)
 {
 
-	int m = n % 10;
+	int m;
 
-	return (m);
+	if (n < 0)
+	{
+		n = (-1) * n;
+		_putchar((n % 10) + '0');
+
+	}
+	m = n % 10;
+	_putchar((m % 10) + '0');
 }
