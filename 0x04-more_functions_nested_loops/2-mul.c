90eae67fd5d9252983bce29a73e808ab6b463450
reubenmwenda <mwendaben97@gmail.com>
2022-10-23 22:04:39 -0700

    2-mul.c has been created

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: I mean it does something
+ * @b: I mean it does something
+ *
+ * Return: result is returned
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 0-isupper.c checks for upper case letters
 1-isdigit.c checks for digits
+2-mul.c multiplies 2 integers
