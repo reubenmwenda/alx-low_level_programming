e6e55f6b8ae124acd69f57ed49787161336d17c9
reubenmwenda <mwendaben97@gmail.com>
2022-10-24 05:30:04 -0700

    6-print_line.c has been created

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - prints lines
+ * @n: it does something
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int k;
+
+	for (k = 0; k < n; k++)
+	{
+		_putchar(95);
+	}
+	return (0);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -4,3 +4,4 @@
 3-print_numbers.c prints 0 to 9
 4-print_most_numbers.c prints 0 to 9 minus 2 and 4
 5-more_numbers.c prints 0 to 14
+6-print_line.c prints lines
