d93ead3105ebc671afe03290391aa39dca224c1f
reubenmwenda <mwendaben97@gmail.com>
2022-10-25 07:45:16 -0700

    0-reset_to_98.c has been created

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+
+/**
+ * reset_to_98 - changes value of n
+ * @n: it does something
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This is for pointers arrays and strings
+0-reset_to_98.c changes values of int n through a pointer
