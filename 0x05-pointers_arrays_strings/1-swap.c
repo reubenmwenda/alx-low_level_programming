eefbab83d7253fb86ea5e559e302c5ec023fa44f
reubenmwenda <mwendaben97@gmail.com>
2022-10-25 08:39:23 -0700

    1-swap.c has been created

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+
+/**
+ * swap_int - checks long of string
+ * @a: it does something
+ * @b: it does something
+ * Return: returns value of string
+ */
+
+void swap_int(int *a, int *b)
+{
+
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 This is for pointers arrays and strings
 0-reset_to_98.c changes values of int n through a pointer
 2-strlen.c checks for size of string
+1-swap.c swaps values
